3b9ee99251432f775fe2fccf457ca76176d6ca5c
Martin Petrasch <martin.petrasch@zephir.ch>
2017-05-30 10:05:03 +0200

    correct model paths

--- src/admin/controllers/ContactController.php
+++ src/admin/controllers/ContactController.php
@@ -12,5 +12,5 @@ class ContactController extends \luya\admin\ngrest\base\Controller
     /**
      * @var string The path to the model which is the provider for the rules and fields.
      */
-    public $modelClass = 'app\modules\addressbook\models\Contact';
+    public $modelClass = 'luya\addressbook\models\Contact';
 }
--- src/admin/controllers/GroupController.php
+++ src/admin/controllers/GroupController.php
@@ -12,5 +12,5 @@ class GroupController extends \luya\admin\ngrest\base\Controller
     /**
      * @var string The path to the model which is the provider for the rules and fields.
      */
-    public $modelClass = 'app\modules\addressbook\models\Group';
+    public $modelClass = 'luya\addressbook\models\Group';
 }
